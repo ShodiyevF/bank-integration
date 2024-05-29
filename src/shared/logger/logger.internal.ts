34506bddfb9d85e5113479ffdc48eb46592c6257
Fayzulloh <fayzulloh6757@gmail.com>
2024-05-29 12:23:28 +0500

    handle logger

--- src/shared/logger/logger.internal.ts
+++ src/shared/logger/logger.internal.ts
@@ -1,34 +1,13 @@
-import { getErrorLine, getCurrentDateFormatted, getCurrentTimeFormatted } from '@sharedLib/helper';
-import { randomUUID } from 'crypto';
-import path from 'path';
-import fs from 'fs';
+import { internalErrors } from '@database/schema';
+import { getErrorLine } from '@sharedLib/helper';
+import { db } from '@database/pg';
 
-export function internalErrorCatcher(error: any){
-    const nowDate = getCurrentDateFormatted();
-    const nowTime = getCurrentTimeFormatted();
-    
+export async function internalErrorCatcher(error: any): Promise<void>{
     const errorFile = getErrorLine(error);
-
-    const checkIfFileExistsError = fs.existsSync(path.join(process.cwd(), `/log/internalError/${nowDate}.json`));
-    if (!checkIfFileExistsError) {
-        fs.writeFileSync(path.join(process.cwd(), `/log/internalError/${nowDate}.json`), JSON.stringify([]));
-    }
-
-    let newLog = {
-        log_id: randomUUID(),
-        log_type: 'error',
-        log_module: 'internalError',
-        log_time: nowTime,
-        log_date: nowDate,
-        log_error_description: error instanceof Error ? errorFile : error,
-        log_error_stack: error.stack,
-    };
-
-    let readFile: string = fs.readFileSync(path.join(process.cwd(), `/log/internalError/${nowDate}.json`), { encoding: 'utf-8' });
     
-    let parsedReadFile = JSON.parse(readFile)
-    
-    parsedReadFile.push(newLog);
-    
-    fs.writeFileSync(path.join(process.cwd(), `/log/internalError/${nowDate}.json`), JSON.stringify(parsedReadFile));
+    await db.insert(internalErrors)
+    .values({
+        ieDescription: error instanceof Error ? errorFile : error,
+        ieStack: error.stack
+    })
 }
