322252c21b7841089dd15049cb32835c411cd009
Fayzulloh <fayzulloh6757@gmail.com>
2024-05-29 12:23:04 +0500

    add logger schema

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,4 +1,6 @@
-import { boolean, pgTable, smallint, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { boolean, json, pgTable, smallint, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+const requestTypes: [string, ...string[]] = [ "SUCCESS", "ERROR" ]
 
 export const users = pgTable('users', {
     userId: uuid('user_id').defaultRandom().primaryKey(),
@@ -7,4 +9,24 @@ export const users = pgTable('users', {
     userGender: boolean('user_gender').default(true),
     userPhoneNumber: varchar('user_phone_number', { length: 13}).notNull().unique(),
     userCreatedAt: timestamp('user_created_at').notNull().defaultNow()
+})
+
+export const requests = pgTable('requests', {
+    requestId: uuid('request_id').defaultRandom().primaryKey(),
+    requestType: varchar('request_type', { enum: requestTypes }).notNull(),
+    requestMethod: varchar('request_method', { length: 32 }).notNull(),
+    requestRoute: text('request_route').notNull(),
+    requestHost: text('request_host').notNull(),
+    requestUserAgent: text('request_user_agent').notNull(),
+    requestBody: json('request_body').notNull(),
+    requestResponseStatus: smallint('request_response_status').notNull(),
+    requestResponseBody: text('request_response_body').notNull(),
+    requestCreatedAt: timestamp('request_created_at').notNull().defaultNow(),
+})
+
+export const internalErrors = pgTable('internal_errors', {
+    ieId: uuid('ie_id').defaultRandom().primaryKey(),
+    ieDescription: text('ie_description').notNull(),
+    ieStack: text('ie_stack').notNull(),
+    ieCreatedAt: timestamp('ie_created_at').notNull().defaultNow(),
 })
