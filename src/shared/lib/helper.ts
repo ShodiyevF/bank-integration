bf3689e127466fa4693999f212e31af38e81a5d6
Fayzulloh <fayzulloh6757@gmail.com>
2024-05-29 12:22:55 +0500

    remove trash helpers

--- src/shared/lib/helper.ts
+++ src/shared/lib/helper.ts
@@ -1,21 +1,3 @@
-function getCurrentDateFormatted() {
-    const today = new Date();
-    const day = String(today.getDate()).padStart(2, '0');
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const year = today.getFullYear();
-
-    return day + '.' + month + '.' + year;
-}
-
-function getCurrentTimeFormatted() {
-    const now = new Date();
-    const hours = String(now.getUTCHours()).padStart(2, '0');
-    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
-    const seconds = String(now.getUTCSeconds()).padStart(2, '0');
-
-    return hours + ':' + minutes + ':' + seconds;
-}
-
 function getErrorLine(error: any) {
     const stackLines = error.stack.split('\n');
     if (stackLines && stackLines.length >= 2) {
@@ -25,7 +7,5 @@ function getErrorLine(error: any) {
 }
 
 export {
-    getCurrentDateFormatted,
-    getCurrentTimeFormatted,
     getErrorLine,
 };
