2c665e41b3faec6c6a39439138b0c64d9ecbb9e1
Fayzulloh <fayzulloh6757@gmail.com>
2024-05-30 14:33:19 +0500

    complete db schema

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,14 +1,26 @@
-import { boolean, json, pgTable, smallint, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { integer, json, pgTable, smallint, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 const requestTypes: [string, ...string[]] = [ "SUCCESS", "ERROR" ]
 
-export const users = pgTable('users', {
-    userId: uuid('user_id').defaultRandom().primaryKey(),
-    userName: varchar('user_name', { length: 36}).notNull(),
-    userAge: smallint('user_age').notNull(),
-    userGender: boolean('user_gender').default(true),
-    userPhoneNumber: varchar('user_phone_number', { length: 13}).notNull().unique(),
-    userCreatedAt: timestamp('user_created_at').notNull().defaultNow()
+export const debtors = pgTable('debtors', {
+    deptorId: uuid('deptor_id').defaultRandom().primaryKey(),
+    deptorPinfl: varchar('deptor_pinfl', { length: 16 }).notNull(),
+    deptorFirstName: varchar('deptor_first_name', { length: 32 }).notNull(),
+    deptorLastName: varchar('deptor_last_name', { length: 32 }).notNull(),
+    deptorMiddleName: varchar('deptor_middle_name', { length: 32 }).notNull(),
+    deptorBornDate: varchar('deptor_born_date', { length: 10 }).notNull(),
+    deptorPassportSeries: varchar('deptor_passport_series', { length: 2 }).notNull(),
+    deptorPassportNumber: integer('deptor_passport_number').notNull(),
+    deptorPassportDate: varchar('deptor_passport_date', { length: 10 }).notNull(),
+    deptorCreatedAt: timestamp('deptor_created_at').notNull().defaultNow()
+})
+
+export const debts = pgTable('debts', {
+    deptId: uuid('dept_id').defaultRandom().primaryKey(),
+    deptBranchId: integer('dept_branch_id').notNull(),
+    deptContractId: integer('dept_contract_id').notNull(),
+    deptSum: integer('dept_sum').notNull(),
+    deptCreatedAt: timestamp('dept_created_at').notNull().defaultNow()
 })
 
 export const requests = pgTable('requests', {
